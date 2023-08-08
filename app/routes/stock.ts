75f42e61a8b8f21d30a1f8ca9afe6f53c5af63f5
szoelloesir <roland.szollosi.mail@gmail.com>
2023-08-08 17:30:12 +0200

    Fix issue with put and triggering data fetching

--- app/routes/stock.ts
+++ app/routes/stock.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getData } from '../controllers/stock';
+import { getData, postTrigger } from '../controllers/stock';
 
 const router = Router();
 
 router.get('/:symbol', getData); // TODO: Use validators (is symbol existing) and maybe add interface for req
-router.put('/:symbol', getData);
+router.put('/:symbol', postTrigger);
 
 export default router;
