f55aa3d584e611803646cd250424b8b09053a0ef
szoelloesir <roland.szollosi.mail@gmail.com>
2023-08-08 08:44:25 +0200

    Implement controller methods

--- app/controllers/stock.ts
+++ app/controllers/stock.ts
@@ -1,7 +1,24 @@
+import CandleDataBase from "../database/candleDataBase";
+import dataFetcher from "../utils/dataFetcher";
+import caluclateSimpleMovingAverage from "../utils/smaCalculator";
+
 export const getData = (req: any, res: any) => {
-    res.json({ message: 'This will return your moving average of: '+ req.params.symbol });
+    const lastDataPoints = CandleDataBase.loadLast10Candle(req.params.symbol);
+    const currentPrice = lastDataPoints && (lastDataPoints?.length ?? 0 > 0)
+      ? lastDataPoints.slice(-1)[0].close
+      : undefined;
+
+    res.json({
+        symbol: req.params.symbol,
+        data: {
+            currentPrice: currentPrice,
+            lastExecution: dataFetcher.getLastExecutionTimeForSymbol(req.params.symbol),
+            sma: caluclateSimpleMovingAverage(lastDataPoints?? [], 10),
+        }
+    })
 }
 
 export const postTrigger = (req: any, res: any) => {
+    dataFetcher.startFetcher(req.params.symbol);
     res.json({ message: 'Your data polling started for: '+ req.params.symbol });
-}
+}
